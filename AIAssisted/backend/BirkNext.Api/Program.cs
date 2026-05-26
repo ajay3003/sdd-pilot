45453090d059d6844b69ee695ee24242b8dc2085
Ajay Anand <ajay.anand@bufdir.no>
2026-05-26 17:22:04 +0200

    Add new feature

--- AIAssisted/backend/BirkNext.Api/Program.cs
+++ AIAssisted/backend/BirkNext.Api/Program.cs
@@ -38,6 +38,12 @@ builder.Services
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 app.UseCors("Frontend");
 app.UseMiddleware<CorrelationIdMiddleware>();
 
